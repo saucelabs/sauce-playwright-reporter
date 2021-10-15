1e7b83c053abebc73e50bea9f6ba64d579e22c33
Félix P <felix@saucelabs.com>
2021-10-15 15:57:30 -0700

    Add integration tests

---
+++ tests/integration/playwright.config.ts
@@ -0,0 +1,27 @@
+import { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  reporter: [
+    ['@saucelabs/playwright-reporter', {
+      region: 'us-west-1',
+      buildName: 'Playwright Integration Tests',
+      tags: [
+        'playwright',
+        'demo',
+        'e2e'
+      ],
+    }],
+  ],
+  testDir: 'tests',
+  use: {
+    video: 'on',
+  },
+  projects: [{
+    name: 'Chromium Suite',
+    use: {
+      browserName: 'chromium',
+    },
+  }],
+};
+
+module.exports = config;
