b78deecdb568398f39a3beca5c7fe8b1a9bfb6cd
Tian Feng <tian.feng@saucelabs.com>
2024-06-06 16:16:25 -0700

    build: Use node 22

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+const ts = require('typescript-eslint');
+const js = require('@eslint/js');
+const prettier = require('eslint-config-prettier');
+const jest = require('eslint-plugin-jest');
+
+module.exports = ts.config(
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  prettier,
+  {
+    plugins: {
+      jest: jest,
+    },
+    rules: {
+      'no-undef': 'off',
+    },
+  },
+  {
+    ignores: ['lib/**'],
+  },
+  {
+    files: ['**/*.js', '**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-var-requires': 'off',
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        __dirname: true,
+        console: true,
+        exports: true,
+        module: true,
+        require: true,
+      },
+    },
+  },
+);
