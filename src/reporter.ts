7de96298f410c961ff7d7e6de2030c4c8b48336d
Tian Feng <tian.feng@saucelabs.com>
2024-08-07 10:15:53 -0700

    fix: Remove ANSI color from error output (#50)
    * fix: Remove ANSI color from error output
    
    * rename

--- src/reporter.ts
+++ src/reporter.ts
@@ -356,6 +356,15 @@ export default class SauceReporter implements Reporter {
     return consoleLog;
   }
 
+  removeAnsiColors(str: string): string {
+    const ansiRegex = new RegExp(
+      // eslint-disable-next-line no-control-regex
+      '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))',
+      'g',
+    );
+    return str.replace(ansiRegex, '');
+  }
+
   constructSauceSuite(rootSuite: PlaywrightSuite) {
     const suite = new SauceSuite(rootSuite.title);
     const assets: Asset[] = [];
@@ -393,7 +402,7 @@ export default class SauceReporter implements Reporter {
             : Status.Failed,
         duration: lastResult.duration,
         output: lastResult.error
-          ? this.errorToMessage(lastResult.error)
+          ? this.removeAnsiColors(this.errorToMessage(lastResult.error))
           : undefined,
         startTime: lastResult.startTime,
         code: new TestCode(lines),
